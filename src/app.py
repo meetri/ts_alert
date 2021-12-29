cf90edc5a68b9f0240c180864fd619eb80ceb491
Demetrius <meetri@gmail.com>
2021-12-29 01:15:51 -0800

    more cleanup

--- src/app.py
+++ src/app.py
@@ -5,16 +5,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-SECURE_KEY = environ.get("secure_key", "abc123")
+SECURE_KEY = environ.get("secure_key")
 bot_token = environ.get("bot_token")
 bot_chatID = environ.get("chat_id")
 
 
 def notify_channel(alertname, symbol, payload):
-    print(alertname)
-    print(symbol)
-    print(payload)
-
     bot_message = f"*Trendspider Alert*: `{symbol}` {alertname} - {payload}"
     telegram_url = f"https://api.telegram.org/bot{bot_token}/"
     message = f'sendMessage?chat_id={bot_chatID}&parse_mode=Markdown&text={bot_message}'
@@ -23,11 +19,13 @@ def notify_channel(alertname, symbol, payload):
     r = response.json()
     print(r)
 
+    return r
+
 
 @app.route('/robot/ts/trigger', methods=['POST'])  # Create main page of web-application
 def ts_trigger():
 
-    securekey = request.args.get('secure', SECURE_KEY)
+    securekey = request.args.get('secure_key')
     alertname = request.args.get('name')
     symbol = request.args.get('symbol')
     note = request.args.get('note')

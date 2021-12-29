15c516633423c82715d97492e06e2bd0e14b3243
Demetrius <meetri@gmail.com>
2021-12-28 23:05:10 -0800

    dump post data

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 import requests
-from flask import Flask, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/') # Create main page of web-application
+
+@app.route('/')  # Create main page of web-application
 def index():
-    return "Welcome to my API!" # Display text on main page
+    data = request.get_data()
+    print(data)
+    return f"Welcome to my API! - {data}"
 
 
 if __name__ == '__main__':
-    app.run() # Run the application
+    app.run()

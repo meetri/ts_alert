2e0f21863a9a70cceab6e07d2b06ff7cb0007996
Demetrius <meetri@gmail.com>
2022-01-03 23:17:45 -0800

    add greeting

--- src/app.py
+++ src/app.py
@@ -8,8 +8,23 @@ app = Flask(__name__)
 SECURE_KEY = environ.get("secure_key")
 bot_token = environ.get("bot_token")
 bot_chatID = environ.get("chat_id")
-PORT = environ.get("PORT")
+PORT = int(environ.get("PORT", "5000"))
 
+def notify_integromat_direct(symbol, alertname, alertnote):
+    url = "https://hook.integromat.com/eto368pdouscc7hvxfyoy38fj1yvcsrq"
+    r = requests.post(f"{url}?symbol={symbol}", data={
+        "symbol": symbol,
+        "alertname": alertname,
+        "alertnote": alertnote
+    })
+    print(r.text)
+
+def notify_integromat(symbol, alertname, alertnote):
+    url = "https://hook.integromat.com/eto368pdouscc7hvxfyoy38fj1yvcsrq"
+    r = requests.post(
+            f"{url}?symbol={symbol}&name={alertname}&note={alertnote}",
+            data=alertnote)
+    print(r.text)
 
 def notify_channel(alertname, symbol, payload):
     bot_message = f"*Trendspider Alert*: `{symbol}` {alertname} - {payload}"
@@ -22,6 +37,11 @@ def notify_channel(alertname, symbol, payload):
     print(r)
 
     return r
+
+
+@app.route('/', methods=['GET'])  # Create main page of web-application
+def greeting():
+    return f"Hello"
 
 
 @app.route('/robot/ts/trigger', methods=['POST'])  # Create main page of web-application
@@ -37,7 +57,8 @@ def ts_trigger():
         payload = request.get_data(as_text=True)
 
     if securekey == SECURE_KEY:
-        notify_channel(alertname, symbol, payload)
+        notify_integromat(symbol, alertname, payload)
+        # notify_channel(alertname, symbol, payload)
         print(f"name={alertname}, symbol={symbol}, note={note}, payload={payload}")
 
     return f"Welcome to my API! - {payload}/{symbol}"
@@ -45,6 +66,7 @@ def ts_trigger():
 
 print(f"starting app: [{__name__}]:{PORT}")
 if __name__ == '__main__':
-    notify_channel("", "success", "Trendspider chart notifier installed")
-    app.run(host='0.0.0.0', port=PORT)
+    # notify_channel("", "success", "Trendspider chart notifier installed")
+    notify_integromat("success", "myalert", "Trendspider notifier installed")
+    app.run(host="0.0.0.0", port=PORT)
     print("why did it exit?")
